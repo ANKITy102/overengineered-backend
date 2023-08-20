2c62579e670b3606fe3d3dc18a5830fb1b1607ce
Ankit Yadav <119873198+ANKITy102@users.noreply.github.com>
2023-08-20 23:30:43 +0530

    Package file changed

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const errorMiddleware_1 = require("./middleware/errorMiddleware");
+const userRoute_1 = __importDefault(require("./routes/userRoute"));
+const blogRoute_1 = __importDefault(require("./routes/blogRoute"));
+const commentRoute_1 = __importDefault(require("./routes/commentRoute"));
+const cors_1 = __importDefault(require("cors"));
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv").config();
+const app = express_1.default();
+app.use(cookieParser());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(cors_1.default({
+    origin: "http://localhost:3000", credentials: true
+}));
+const PORT = 3090;
+app.get("/", (req, res) => {
+    return res.send("Hi there");
+});
+app.use("/api/users", userRoute_1.default);
+app.use("/api/blogs", blogRoute_1.default);
+app.use("/api/comments", commentRoute_1.default);
+app.use(errorMiddleware_1.errorHandler);
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors({
   origin:"http://localhost:3000", credentials : true }));
 
-const PORT =  5000;
+const PORT =  3090;
 app.get("/", (req:Request,res:Response)=>{
   return res.send("Hi there")
 })
